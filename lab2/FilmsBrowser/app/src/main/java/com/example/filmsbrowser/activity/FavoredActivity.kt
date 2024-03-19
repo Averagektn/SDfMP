3c264991af7ed489a58ea4baa8d0b9c97a5f5bb5
Никита Глушаченко <rylonoboev2004@gmail.com>
2024-03-19 19:04:47 +0300

    added favored activity

---
+++ lab2/FilmsBrowser/app/src/main/java/com/example/filmsbrowser/activity/FavoredActivity.kt
@@ -0,0 +1,12 @@
+package com.example.filmsbrowser.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.filmsbrowser.R
+
+class FavoredActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favored)
+    }
+}
