2624e8836603066d55616e1e8de0a570c8e28339
Glushachenko Nikita <90308369+Averagektn@users.noreply.github.com>
2024-03-22 19:13:22 +0200

    Add files via upload

---
+++ mycoolfilms/mycoolfilms/mycoolfilmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  mycoolfilmsApp.swift
+//  mycoolfilms
+//
+//  Created by name on 22/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct mycoolfilmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
