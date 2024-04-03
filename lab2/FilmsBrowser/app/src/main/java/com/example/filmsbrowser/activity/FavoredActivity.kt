de09877a40fd66556f11d27c51d8e79f5d4d3458
Никита Глушаченко <rylonoboev2004@gmail.com>
2024-04-03 19:48:48 +0300

    Favored is MVVM

--- lab2/FilmsBrowser/app/src/main/java/com/example/filmsbrowser/activity/FavoredActivity.kt
+++ lab2/FilmsBrowser/app/src/main/java/com/example/filmsbrowser/activity/FavoredActivity.kt
@@ -4,22 +4,21 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.filmsbrowser.adapter.FavoredAdapter
 import com.example.filmsbrowser.databinding.ActivityFavoredBinding
 import com.example.filmsbrowser.model.Film
+import com.example.filmsbrowser.viewModel.FavoredViewModel
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
 
 class FavoredActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     private lateinit var database: FirebaseDatabase
     private lateinit var binding: ActivityFavoredBinding
     private lateinit var favoredAdapter: FavoredAdapter
-    private lateinit var films: ArrayList<Film>
+    private lateinit var favoredViewModel: FavoredViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,57 +40,15 @@ class FavoredActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        loadFilms()
-    }
+        favoredViewModel = ViewModelProvider(this)[FavoredViewModel::class.java]
 
-    private fun loadFilms() {
-        films = ArrayList()
-
-        database.getReference("favored/${auth.currentUser!!.uid}").addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                loadFavored(snapshot)
-            }
-
-            override fun onCancelled(p0: DatabaseError) {
-                Toast.makeText(this@FavoredActivity, "Check your Internet connection", Toast.LENGTH_LONG).show()
-            }
-        })
-    }
-
-    private fun loadFavored(snapshot: DataSnapshot) {
-        val favoredList = ArrayList<String>()
-
-        for (childSnapshot in snapshot.children) {
-            val listItem = childSnapshot.getValue(String::class.java)
-
-            if (listItem != null) {
-                favoredList.add(listItem)
-            }
+        favoredViewModel.getFavoredFilms(auth.currentUser!!.uid).observe(this) { films ->
+            favoredAdapter = FavoredAdapter(this@FavoredActivity, films as ArrayList<Film>)
+            binding.filmsList.adapter = favoredAdapter
         }
 
-        val filmsRef = database.getReference("films")
-        filmsRef.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                films.clear()
-
-                for (elem in snapshot.children) {
-                    if (favoredList.contains(elem.key)) {
-                        val model = elem.getValue(Film::class.java)
-
-                        if (model != null) {
-                            model.id = elem.key!!
-                            films.add(model)
-                        }
-                    }
-                }
-
-                favoredAdapter = FavoredAdapter(this@FavoredActivity, films)
-                binding.filmsList.adapter = favoredAdapter
-            }
-
-            override fun onCancelled(p0: DatabaseError) {
-                Toast.makeText(this@FavoredActivity, "Check your Internet connection", Toast.LENGTH_LONG).show()
-            }
-        })
+        favoredViewModel.getErrorMessage().observe(this) { errorMessage ->
+            Toast.makeText(this@FavoredActivity, errorMessage, Toast.LENGTH_LONG).show()
+        }
     }
 }
