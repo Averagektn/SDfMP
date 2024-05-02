13b43847bbe3e58eeec6e72250698aa51d5863a6
AverageKtn <rylonoboev2004@gmail.com>
2024-05-02 23:16:22 +0300

    started xamarin

---
+++ lab4/FilmsBrowser/FilmsBrowser/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using FilmsBrowser.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace FilmsBrowser.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
