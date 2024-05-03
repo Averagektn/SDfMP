952ada2d3d1ac8ce274cb2b3696af17f969472f3
AverageKtn <rylonoboev2004@gmail.com>
2024-05-04 02:18:08 +0300

    Created slider

--- lab4/FilmsBrowser/FilmsBrowser/Views/FilmDetailPage.xaml.cs
+++ lab4/FilmsBrowser/FilmsBrowser/Views/FilmDetailPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using FilmsBrowser.ViewModels;
+﻿using FilmsBrowser.Config;
+using FilmsBrowser.ViewModels;
+using Firebase.Storage;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace FilmsBrowser.Views
@@ -13,10 +17,30 @@ namespace FilmsBrowser.Views
             BindingContext = _viewModel = new FilmDetailViewModel();
         }
 
-        protected override void OnAppearing()
+        protected override async void OnAppearing()
         {
             base.OnAppearing();
             _viewModel.OnAppearing();
+
+            var storage = new FirebaseStorage(MyFirebaseConfig.StorageLink, new FirebaseStorageOptions
+            {
+                AuthTokenAsyncFactory = () => Task.FromResult(MyFirebaseConfig.WebApiKey)
+            });
+
+            try
+            {
+                var downloadUrl = await storage
+                    .Child("film_images")
+                    .Child(_viewModel.FilmId)
+                    .Child(_viewModel.FilmId)
+                    .GetDownloadUrlAsync();
+
+                Carousel.ItemsSource = new ObservableCollection<Image>()
+                {
+                    new Image() { Source = downloadUrl }
+                };
+            }
+            catch { }
         }
     }
 }
